98301d2e7dc6d8166635aee0c1cd399174e4a575
Joe Martin <martin@360works.com>
2022-07-16 17:30:58 -0400

    Initial commit - initializing Spring Project

---
+++ src/main/java/com/joevmartin/spring/springrecipelist/SpringRecipeListApplication.java
@@ -0,0 +1,13 @@
+package com.joevmartin.spring.springrecipelist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRecipeListApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringRecipeListApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/joevmartin/spring/springrecipelist/SpringRecipeListApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joevmartin.spring.springrecipelist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRecipeListApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
